ac29751a3d887da2bb56cb090f4c60fbe6a92aa9
bai0723 <bairainyang@163.com>
2020-08-20 17:25:55 +0800

    add some gadget

---
+++ Some gadgets/Match the IP address in the file.py
@@ -0,0 +1,25 @@
+﻿import re
+
+def find_ip(port):
+    f = open('exc.txt','r')
+    while True:
+        data =""
+        for line in f:
+            if line == "\n":
+                break
+            data += line
+        if not data:
+            return "没有找到该端口"
+        obj = re.match(r'\S+',data)
+        if port == obj.group():
+            pattern = r'[0-9a-f]{4}\.[0-9a-f]{4}\.[0-9a-f]{4}'#匹配虚拟接口
+            pattern1 = r'[1-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/[0-9]{1,2}|Unknown'
+            obj = re.search(pattern1,data)
+            if obj:
+                return obj.group()
+
+
+if __name__ == '__main__':
+    while True:
+        port = input("请输入端口号")
+        print(find_ip(port))
